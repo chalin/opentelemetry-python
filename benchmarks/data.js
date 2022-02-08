80d18f165c2df12a2e35bc3c8e9921f863f9c40d
github-action-benchmark <github@users.noreply.github.com>
2022-02-08 16:37:13 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - sdk (pytest) benchmark result for fd9862aee1ef305fca39879931c92429d57106d6

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644338227206,
+  "lastUpdate": 1644338233441,
   "repoUrl": "https://github.com/chalin/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -65172,6 +65172,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000017598404793064472",
             "extra": "mean: 58.71780844548008 usec\nrounds: 9141"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "owais@users.noreply.github.com",
+            "name": "Owais Lone",
+            "username": "owais"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fd9862aee1ef305fca39879931c92429d57106d6",
+          "message": "Update opentelemetry-sdk min compatible version (#2442)",
+          "timestamp": "2022-02-06T01:04:17+05:30",
+          "tree_id": "b48be286ed44d6a2deba6f08919f283442a5c3c4",
+          "url": "https://github.com/chalin/opentelemetry-python/commit/fd9862aee1ef305fca39879931c92429d57106d6"
+        },
+        "date": 1644338217310,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 28067.716596160957,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000012219908690391517",
+            "extra": "mean: 35.62812089020373 usec\nrounds: 5079"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 20303.32003374697,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000002029443480470357",
+            "extra": "mean: 49.25302848686124 usec\nrounds: 5757"
           }
         ]
       }
