67fdf49b8a088fcb260dd9870e6f21439df06ddc
github-action-benchmark <github@users.noreply.github.com>
2022-03-03 12:06:08 +0000

    add OpenTelemetry Python Benchmarks - Python 3.7 - sdk (pytest) benchmark result for 50093f220f945ae38e769ab539c78c975e582bef

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646309142524,
+  "lastUpdate": 1646309168269,
   "repoUrl": "https://github.com/chalin/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -65132,6 +65132,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000002023199997345816",
             "extra": "mean: 60.71999937231121 usec\nrounds: 6374"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "12352919+TylerHelmuth@users.noreply.github.com",
+            "name": "Tyler Helmuth",
+            "username": "TylerHelmuth"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "50093f220f945ae38e769ab539c78c975e582bef",
+          "message": "Fix OTLP HTTP Endpoint Usage (#2493)",
+          "timestamp": "2022-03-02T14:06:28-08:00",
+          "tree_id": "25b24a9530dc5388c57b8fc2476658596c7cc30f",
+          "url": "https://github.com/chalin/opentelemetry-python/commit/50093f220f945ae38e769ab539c78c975e582bef"
+        },
+        "date": 1646309150323,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 21875.807568495275,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000010456385347932584",
+            "extra": "mean: 45.712598123241996 usec\nrounds: 4581"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 16243.729309562117,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000017138191181603894",
+            "extra": "mean: 61.562217699068334 usec\nrounds: 5650"
           }
         ]
       }
